7f2dce2432f144cb9469ff3cba48bb7f72940ae9
Kayathiri-Mahendrakumaran <kayathirikaya33@gmail.com>
2020-02-19 09:38:09 +0530

    alter code alter code alter visualization alter data alter code

---
+++ gsmote/GSMOTE.py
@@ -0,0 +1,52 @@
+from collections import Counter
+from gsmote import GeometricSMOTE
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+import numpy as np
+import sys
+sys.path.append('../../')
+
+def OverSample():
+    # X, y = make_classification(n_classes=2, class_sep=2,
+    # weights=[0.1, 0.9], n_informative=3, n_redundant=1, flip_y=0,
+    # n_features=20, n_clusters_per_class=1, n_samples=1000, random_state=10)
+    df = pd.read_csv('data/adult.csv')
+    # Loading of Selected Features into X
+    X = df.iloc[:, [0, 1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12]].values
+
+    columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(), [1])], remainder='passthrough')
+    X = np.array(columnTransformer.fit_transform(X), dtype=np.str)
+    X = X[:, 1:]
+    columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(), [10])], remainder='passthrough')
+    X = np.array(columnTransformer.fit_transform(X), dtype=np.str)
+    X = X[:, 1:]
+    columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(), [26])], remainder='passthrough')
+    X = np.array(columnTransformer.fit_transform(X), dtype=np.str)
+    X = X[:, 1:]
+    columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(), [32])], remainder='passthrough')
+    X = np.array(columnTransformer.fit_transform(X), dtype=np.str)
+    X = X[:, 1:]
+    columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(), [46])], remainder='passthrough')
+    X = np.array(columnTransformer.fit_transform(X), dtype=np.str)
+    X = X[:, 1:]
+    columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(), [51])], remainder='passthrough')
+    X = np.array(columnTransformer.fit_transform(X), dtype=np.str)
+    X = X[:, 1:]
+    sc_X = StandardScaler()
+    X = sc_X.fit_transform(X)
+    # Loading of the Label into y
+    columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(), [-1])], remainder='passthrough')
+    y1 = np.array(columnTransformer.fit_transform(df), dtype=np.str)
+    y = y1[:, 1]
+    y = np.where(y == "0.0", "0", y)
+    y = np.where(y == "1.0", "1", y)
+
+
+
+    print('Original dataset shape %s' % Counter(y))
+    gsmote = GeometricSMOTE(random_state=1)
+    X_res, y_res = gsmote.fit_resample(X, y)
+    print('Resampled dataset shape %s' % Counter(y_res))
+    return X_res,y_res
--- visualize.py
+++ visualize.py
@@ -5,21 +5,32 @@ from matplotlib import colors
 
 
 def show_gsom(output, max_count,index_col,label_col):
-    listed_color_map = _get_color_map(max_count, alpha=0.9)
+    #listed_color_map = _get_color_map(max_count, alpha=0.9)
 
     fig, ax = plt.subplots()
     for index, i in output.iterrows():
         x=i['x']
         y=i['y']
         if i['hit_count']>0:
-            c='red'
-            label = ", ".join(map(str,i[index_col]))
-
+            #c='red'
+            #label = ", ".join(map(str,i[index_col]))
+            count_0 = 0
+            count_1 = 0
+            label = ""
+            for id in i[index_col]:
+                if id == '1':
+                    count_1 += 1
+                if id == '0':
+                    count_0 += 1
+            if count_1 != 0:
+                label = label + "1(" + str(count_1) + ")"
+            if count_0 != 0:
+                label = label + "0(" + str(count_0) + ")"
         else:
             label = ""
-            c='yellow'
-        ax.plot(x,y, 'o', color=listed_color_map.colors[i['hit_count']],markersize=2)
-        ax.annotate(label, (x, y), fontsize=4)
+            #c='yellow'
+        ax.plot(x,y, 'o', color=findColor(count_0,count_1),markersize=1)
+        ax.annotate(label, (x, y), fontsize=2)
         print("{},{}-{}".format(x, y,label))
 
 
@@ -28,17 +39,27 @@ def show_gsom(output, max_count,index_col,label_col):
     plt.savefig("gsom.png",dpi=1200)
 
 
-def _get_color_map(max_count, alpha=0.5):
+# def _get_color_map(max_count, alpha=0.5):
+#
+#     np.random.seed(1)
+#
+#     cmap = cm.get_cmap('Reds', max_count + 1)  # set how many colors you want in color map
+#     # https://matplotlib.org/examples/color/colormaps_reference.html
+#
+#     color_list = []
+#     for ind in range(cmap.N):
+#         c = []
+#         for x in cmap(ind)[:3]: c.append(x * alpha)
+#         color_list.append(tuple(c))
+#
+#     return colors.ListedColormap(color_list, name='gsom_color_list')
 
-    np.random.seed(1)
 
-    cmap = cm.get_cmap('Reds', max_count + 1)  # set how many colors you want in color map
-    # https://matplotlib.org/examples/color/colormaps_reference.html
-
-    color_list = []
-    for ind in range(cmap.N):
-        c = []
-        for x in cmap(ind)[:3]: c.append(x * alpha)
-        color_list.append(tuple(c))
-
-    return colors.ListedColormap(color_list, name='gsom_color_list')
+def findColor(count_0, count_1):
+    if count_0 > count_1:
+        color = 'blue'
+    elif count_0 < count_1:
+        color = 'red'
+    else:
+        color = 'orange'
+    return color
