2b2fa413be41bb5701e6427899ec23ce3c207dea
Vivek Vinushanth <cvivekvinushanth@gmail.com>
2020-02-20 09:16:48 +0530

    Gradient Boost classifier evaluation added.

--- gsmote/comparison_testing/comparison_test.py
+++ gsmote/comparison_testing/comparison_test.py
@@ -2,6 +2,7 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import GradientBoostingClassifier
 from gsmote.comparison_testing.Evaluator import evalute
 import gsmote.preprocessing as pp
 from gsmote import GSMOTE as gs
@@ -14,7 +15,7 @@ X,y = pp.preProcess(date_file)
 def linear_training(X,y):
     X_t, X_test, y_t, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-    X_train,y_train = X_t,y_t
+    X_train,y_train = gs.OverSample(X_t,y_t)
                                                                                                                    
     # Fitting Simple Linear Regression to the Training set
     regressor = LinearRegression()
@@ -23,11 +24,28 @@ def linear_training(X,y):
     # Predicting the Test set results
     y_predict = regressor.predict(X_test)
     y_pred  = np.where(y_predict>0.5,1,0)
-    print(y_pred)
 
-    evalute(y_test,y_pred)
+    evalute("Linear Regression",y_test,y_pred)
+
+def gradient_boosting(X,y):
+     X_t, X_test, y_t, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+     X_train,y_train = gs.OverSample(X_t,y_t)
+
+     # Fitting Gradient boosting
+     gbc = GradientBoostingClassifier (n_estimators=100, learning_rate = 0.01, max_depth = 3)
+     gbc.fit(X_train, y_train)
+
+     # Predicting the Test set results
+     y_predict = gbc.predict(X_test)
+     y_pred  = np.where(y_predict.astype(int)>0.5,1,0)
+
+     evalute("Gradient Boosting",y_test,y_pred)
+
+
 
 linear_training(X,y)
+gradient_boosting(X,y)
 
 
 
