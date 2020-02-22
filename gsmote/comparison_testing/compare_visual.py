c4c81b99734ae74dbe897e2984d7fae06814c6a3
Kayathiri-Mahendrakumaran <kayathirikaya33@gmail.com>
2020-02-22 12:38:15 +0530

    refactored code

--- gsmote/comparison_testing/compare_visual.py
+++ gsmote/comparison_testing/compare_visual.py
@@ -39,3 +39,30 @@ def visualize_data(X,y,title):
     #plt.show()
     plt.savefig("../../output/compare_" + datetime.datetime.now().strftime("%Y-%m-%d__%H_%M_%S") + ".png", dpi=1200)
 
+def visualize(X):
+    feat_cols = ['pixel' + str(i) for i in range(X.shape[1])]
+    df = pd.DataFrame(X, columns=feat_cols)
+    df['y'] = 1
+    df['label'] = df['y'].apply(lambda i: str(i))
+
+    # For reproducability of the results
+    np.random.seed(42)
+    rndperm = np.random.permutation(df.shape[0])
+
+    pca = PCA(n_components=2)
+    pca_result = pca.fit_transform(df[feat_cols].values)
+    df['pca-one'] = pca_result[:, 0]
+    df['pca-two'] = pca_result[:, 1]
+
+
+
+    from sklearn.cluster import KMeans
+
+    kmeans = KMeans(n_clusters=5)
+    kmeans.fit(pca_result)
+    y_kmeans = kmeans.predict(pca_result)
+    plt.scatter(pca_result[:,0], pca_result[:, 1], c=y_kmeans, s=50, cmap='viridis')
+    centers = kmeans.cluster_centers_
+    plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5);
+    plt.show()
+
--- gsmote/comparison_testing/comparison_test.py
+++ gsmote/comparison_testing/comparison_test.py
@@ -8,51 +8,54 @@ from sklearn.tree import DecisionTreeRegressor
 from gsmote import GSMOTE
 from gsmote.comparison_testing.Evaluator import evaluate
 import gsmote.preprocessing as pp
-from gsmote.comparison_testing.compare_visual import  visualize_data as vs
+from gsmote.comparison_testing.compare_visual import visualize_data as vs
 import sys
+
 sys.path.append('../../')
 
-date_file = "../../data/adultmini.csv".replace('\\','/')
-X,y = pp.preProcess(date_file)
+date_file = "../../data/adult2.csv".replace('\\', '/')
+X, y = pp.preProcess(date_file)
 
-def linear_training(X,y):
-    X_t, X_test, y_t, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+def linear_training(X, y):
+    X_t, X_test, y_t, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
     # Visualize original data
     vs(X_t, y_t, "Original data")
 
     # oversample
-    X_train,y_train = GSMOTE.OverSample(X_t,y_t)
+    X_train, y_train = GSMOTE.OverSample(X_t, y_t)
     # visualize oversampled data
     vs(X_train, y_train, "Oversampled ")
-                                                                                                                   
+
     # Fitting Simple Linear Regression to the Training set
     regressor = LinearRegression()
     regressor.fit(X_train, y_train)
 
     # Predicting the Test set results
     y_predict = regressor.predict(X_test)
-    y_pred  = np.where(y_predict>0.5,1,0)
+    y_pred = np.where(y_predict > 0.5, 1, 0)
 
-    evaluate("Linear Regression",y_test,y_pred)
+    evaluate("Linear Regression", y_test, y_pred)
 
 
-def gradient_boosting(X,y):
-     X_t, X_test, y_t, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+def gradient_boosting(X, y):
+    X_t, X_test, y_t, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-     X_train,y_train = GSMOTE.OverSample(X_t,y_t)
+    X_train, y_train = GSMOTE.OverSample(X_t, y_t)
 
-     # Fitting Gradient boosting
-     gbc = GradientBoostingClassifier (n_estimators=100, learning_rate = 0.01, max_depth = 3)
-     gbc.fit(X_train, y_train)
+    # Fitting Gradient boosting
+    gbc = GradientBoostingClassifier(n_estimators=100, learning_rate=0.01, max_depth=3)
+    gbc.fit(X_train, y_train)
 
-     # Predicting the Test set results
-     y_predict = gbc.predict(X_test)
-     y_pred  = np.where(y_predict.astype(int)>0.5,1,0)
+    # Predicting the Test set results
+    y_predict = gbc.predict(X_test)
+    y_pred = np.where(y_predict.astype(int) > 0.5, 1, 0)
 
-     evaluate("Gradient Boosting",y_test,y_pred)
+    evaluate("Gradient Boosting", y_test, y_pred)
 
-def KNN(X,y):
+
+def KNN(X, y):
     X_t, X_test, y_t, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
     X_train, y_train = GSMOTE.OverSample(X_t, y_t)
     # X_train,y_train = X_t,y_t
@@ -63,7 +66,7 @@ def KNN(X,y):
     # Predicting the Test set results
     y_pred = classifier.predict(X_test).astype(int)
 
-    evaluate("KNN",y_test, y_pred)
+    evaluate("KNN", y_test, y_pred)
 
 
 def decision_tree(X, y):
@@ -81,21 +84,23 @@ def decision_tree(X, y):
 
     evaluate("Decision Tree", y_test, y_pred)
 
-def MLPClassifier(X,y):
+
+def MLPClassifier(X, y):
     X_t, X_test, y_t, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
     X_train, y_train = GSMOTE.OverSample(X_t, y_t)
 
     # Fitting MLPClassifier to the Training set
     from sklearn.neural_network import MLPClassifier
-    mlp = MLPClassifier(hidden_layer_sizes=(10, 10, 10), max_iter=1000,solver='lbfgs', alpha=1e-5,
-                    random_state=1)
+    mlp = MLPClassifier(hidden_layer_sizes=(10, 10, 10), max_iter=1000, solver='lbfgs', alpha=1e-5,
+                        random_state=1)
     mlp.fit(X_train, y_train)
     y_pred = mlp.predict(X_test).astype(int)
 
     evaluate("MLPClassifier", y_test, y_pred)
 
-linear_training(X,y)
-gradient_boosting(X,y)
-KNN(X,y)
-decision_tree(X,y)
-MLPClassifier(X,y)
+
+# linear_training(X, y)
+gradient_boosting(X, y)
+# KNN(X, y)
+# decision_tree(X, y)
+# MLPClassifier(X, y)
